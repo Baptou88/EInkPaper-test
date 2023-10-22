644bd3e4f651004074307e5bba0c524299f01beb
= <mathieu.baptiste88@gmail.com>
2023-10-22 08:35:25 +0200

    http call

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,14 @@
 // uncomment next line to use class GFX of library GFX_Root instead of Adafruit_GFX
 //#include <GFX.h>
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "WifiCredentials.h"
+#include <ArduinoJson.h>
+
 
 #define ENABLE_GxEPD2_GFX 1 
-#include <GxEPD2_BW.h>
+//#include <GxEPD2_BW.h>
 #include <GxEPD2_3C.h>
 //#include <GxEPD2_7C.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
@@ -25,13 +30,16 @@
 // e.g. for Wemos D1 mini:
 //GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4)); // GDEH0154D67
 
+
+String serverName = "http://192.168.1.10/dataEtang";
 class Plot
 {
 private:
  
   int _xstep=50;
   int _data[10] ;
-  int _margin = 5;
+  String label[10] = {"1","2","3","4","5","6","7","8","9","10"} ;
+  int _margin = 20;
   int index = 0;
 public:
   Plot(int x, int y);
@@ -39,9 +47,10 @@ public:
 
   void draw(GxEPD2_GFX& display);
   void add(int y);
-   int _x = 0;
+  void getRect(int *xmin, int *ymin, int *width, int *height);
+  int _x = 0;
   int _y = 0;
-  int _width = 400;
+  int _width = 720;
   int _height = 160;
 
 };
@@ -50,6 +59,14 @@ Plot::Plot(int x, int y)
 {
   _x = x;
   _y = y;
+  // label[0] = "0";
+  // label[1] = "1";
+  // label[2] = "2";
+  // label[3] = "3";
+  // label[4] = "4";
+  // label[5] = "5";
+  // label[6] = "6";
+  // label[7] = "7";
 }
 
 Plot::~Plot()
@@ -61,11 +78,15 @@ void Plot::draw(GxEPD2_GFX& display )
   display.drawRect(_x - _margin, _y - _margin - _height, _width + _margin * 2 , _height + _margin *2, GxEPD_BLACK);
   display.drawLine(_x,_y,_x, _y - _height,GxEPD_BLACK);
   display.drawLine(_x,_y,_x + _width, _y , GxEPD_BLACK);
-  display.drawCircle(_x,_y,_margin,GxEPD_RED);
+  //display.drawCircle(_x,_y,_margin,GxEPD_RED);
   Serial.println(index);
+  display.drawLine(_x - _margin/4,_y-100,_x+_margin/4,_y-100, GxEPD_BLACK);
+  display.drawLine(_x - _margin/4,_y-50,_x+_margin/4,_y-50, GxEPD_BLACK);
   for (size_t i = 0; i < index; i++)
   {
-    display.drawCircle(i*_xstep + _x, _y - _data[i],4,GxEPD_RED );
+    display.fillCircle(i*_xstep + _x, _y - _data[i],4, GxEPD_RED );
+    display.setCursor(i*_xstep + _x,_y+12);
+    display.println(label[i]);
 
     if (i != 0)
     {
@@ -79,8 +100,13 @@ void Plot::draw(GxEPD2_GFX& display )
 
 void Plot::add(int y)
 {
-  if (index > 10)
+  if (index >= 10)
   {
+    for (size_t i = 0; i < 10 -1; i++)
+    {
+      _data[i] = _data[i+1];
+    }
+    _data[9] = y;
     
   } else
   {
@@ -91,9 +117,29 @@ void Plot::add(int y)
   
 }
 
+void Plot::getRect(int *xmin, int *ymin, int *width, int *height)
+{
+  *xmin = _x - _margin;
+  *ymin = _y - _margin - _height;
+  *width = _width + _margin * 2;
+  *height =  _height + _margin *2;
+}
+
 Plot graph(40,448);
+void drawRectByCenter(int x,int y,int w,int h,int r, uint16_t c){
+  //display.drawRoundRect(x-w/2,y-h/2,w,h,r,GxEPD_BLACK);
+  display.fillRoundRect(x-w/2,y-h/2,w,h,r,c);
+}
+void drawLevel(int val){
+  int centerx = 400;
+  int centery = 200;
 
+  drawRectByCenter(centerx,centery, 600, 50,6,GxEPD_BLACK);
+  drawRectByCenter(centerx,centery, 596, 46,6,GxEPD_WHITE);
 
+  display.fillRoundRect(400-298+3, 200-23+3, 50,40,6,GxEPD_BLACK);
+
+}
 const char HelloWorld[] = "Hello World!";
 void helloWorld()
 {
@@ -246,7 +292,10 @@ void testGraph(bool partialUpdate = false) {
   uint16_t y = ((display.height() - tbh) / 2) - tby;
   if (partialUpdate)
   {
-    display.setPartialWindow(graph._x,graph._y-graph._height,graph._width,graph._height);
+    int x,y,w,h;
+    graph.getRect(&x,&y,&w,&h);
+    //display.setPartialWindow(graph._x,graph._y-graph._height,graph._width,graph._height);
+    display.setPartialWindow(x,y,w,h);
   } else
   {
     display.setFullWindow();
@@ -262,7 +311,7 @@ void testGraph(bool partialUpdate = false) {
     display.setCursor(x, y);
     display.print(HelloWorld);
     display.setCursor(x+60, y+60);
-    display.drawLine(50,50,100,100,GxEPD_BLACK);
+    drawLevel(50);
 
     graph.draw(display);
   }
@@ -270,13 +319,36 @@ void testGraph(bool partialUpdate = false) {
 }
 bool requiredUpdate = false;
 
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+unsigned long timerDelay = 600000;
+// Set timer to 10 seconds (5000)
+//unsigned long timerDelay = 30000;
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Hello World !");
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
   graph.add(20);
   graph.add(50);
   graph.add(70);
+  graph.add(40);
+  graph.add(60);
+  graph.add(100);
+  graph.add(95);
+  graph.add(80);
+  graph.add(35);
   delay(5);
   //display.init(115200); // default 10ms reset pulse, e.g. for bare panels with DESPI-C02
   display.init(115200, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
@@ -306,6 +378,50 @@ void loop() {
     requiredUpdate = false;
     testGraph(true);
   }
+
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+
+      String serverPath = serverName ;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverPath.c_str());
+      
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+
+        DynamicJsonDocument doc(1024);
+        deserializeJson(doc,payload);
+        JsonObject obj = doc.as<JsonObject>();
+        long time = obj[String("niveauEtangP")]; 
+        Serial.printf("niveau etang %i\n",time);
+        graph.add(time);
+        requiredUpdate = true;
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+
   
   
 };
